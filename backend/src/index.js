bf78e033f7f94947921e8911b66a3ab3c73d266f
Daniel Sá <danielviana18@gmail.com>
2020-03-27 18:26:38 +0000

    Backend: Done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const routes = require('./routes')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(routes)
 
 app.use(express.json)
+
+app.listen(process.env.PORT || 3000,() => {
+    console.log('Listenning to requests on port 3000');
+});
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,23 @@
 const express = require('express')
-const routes = express().Router
+const routes = express.Router()
+const crypto = require('crypto')
+const connection = require('./database/connection')
 
-module.exports = routes
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
+
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create)
+
+routes.get('/profile', ProfileController.index)
+
+routes.post('/sessions', SessionController.create)
+
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create)
+routes.delete('/incidents/:id', IncidentController.delete)
+
+
+module.exports = routes;
